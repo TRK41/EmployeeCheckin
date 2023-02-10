84bfa6c94f2d7a0d309f3d700e338b4ed7d0d2b8
TRK41 <Troyk@bell.net>
2023-02-09 23:23:23 -0500

    building code in schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT NOT NULL,
-  name VARCHAR(30) NOT NULL
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
 );
 
 CREATE TABLE role (
-  id INT NOT NULL,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL  
+  department_id INT NOT NULL, 
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE 
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
+  id INT AUTO_INCREMENT PRIMARY KEY ,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL, 
-  manager_id INT   
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT,
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
